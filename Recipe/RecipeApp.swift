321c6f3d85bb09ac198d3f7ee53765057cfbea0b
Bhargavi <sbhargavicse27@gmail.com>
2025-01-25 17:16:16 +0530

    Initial commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Jayabharathi S on 24/01/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
